202a92b005794ed8e220ee5316f808765b09803c
aakash-28 <aakash.tiwari28@gmail.com>
2024-05-15 00:31:14 +0530

    progress save on todo app

--- Projects/1-to-do-app-v1/src/App.jsx
+++ Projects/1-to-do-app-v1/src/App.jsx
@@ -1,50 +1,20 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2.jsx";
+import "./App.css";
 
 function App() {
   return (
     <>
       <center className="todo-container">
         <AppName />
-
-        <div class="container text-center">
-          <div class="row">
-            <div class="col-6">
-              <input type="text" placeholder="Enter to do here"></input>
-            </div>
-            <div class="col-4">
-              <input type="date"></input>
-            </div>
-            <div class="col-2">
-              <button type="button" class="btn btn-success">
-                Add
-              </button>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-6">Buy milk</div>
-            <div class="col-4">4/10/2023</div>
-            <div class="col-2">
-              <button type="button" class="btn btn-danger">
-                Delete
-              </button>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-6">Go to college</div>
-            <div class="col-4">4/10/2023</div>
-            <div class="col-2">
-              <button type="button" class="btn btn-danger">
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
+        <AddTodo />
+        <TodoItem1 />
+        <TodoItem2 />
       </center>
     </>
   );
